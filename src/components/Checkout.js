82be99008932a817d6c7e52fcd45d8d9dcde26e8
subhampreet <subhampreet18@gmail.com>
2021-02-03 22:39:58 +0530

    Product Adding Feature

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -2,9 +2,14 @@ import React from 'react';
 import './Checkout.css';
 import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
 import Subtotal from './Subtotal';
+import CheckoutProduct from "./CheckoutProduct";
+import { useStateValue } from "../StateProvider";
 
 
 function Checkout() {
+
+    const [{basket}, dispatch] = useStateValue();
+
     return (
         <div className="checkout">
             <div className="checkout-left">
@@ -12,6 +17,17 @@ function Checkout() {
 
                 <div>
                     <h2 className="checkout-title"><ShoppingCartOutlinedIcon className="checkout-basket-icon" />Your Shopping Cart </h2>
+
+                    {basket.map(item => (
+                        <CheckoutProduct
+                            id = {item.id}
+                            title = {item.title}
+                            image = {item.image}
+                            price = {item.price}
+                            rating = {item.rating}
+                        />
+                    ))}
+
                 </div>
             </div>
 
--- src/components/CheckoutProduct.js
+++ src/components/CheckoutProduct.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import "./CheckoutProduct.css";
+import StarIcon from "@material-ui/icons/Star"; 
 
-function CheckoutProduct() {
+function CheckoutProduct({id, image, title, price, rating}) {
+    const removeFromBasket = () => {
+        
+    }
+
     return (
         <div className="checkoutProduct">
-            
+            <img className="checkoutProduct_image" src={image} />
+
+            <div className="checkoutProduct_info">
+                <p className="checkoutProduct_title">{title}</p>
+                <p className="checkoutProduct_price">
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className="checkoutProduct_rating">
+                    {Array(rating)
+                    .fill()
+                    .map((_, i) => (
+                        <StarIcon className="star-icon" />
+                    ))}
+                </div>
+                <button>Remove from Basket</button>
+            </div>
         </div>
     )
 }
